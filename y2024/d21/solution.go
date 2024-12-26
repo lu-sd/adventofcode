7960071c277f7a49df927913913f08e10fb870bb
lu-sd <luhou.me@gmail.com>
2024-12-26 14:56:07 -0800

    add d21

--- y2024/d21/solution.go
+++ y2024/d21/solution.go
@@ -6,32 +6,15 @@ import (
 	"io"
 	"log"
 	"os"
-	"slices"
+	"strings"
 	"time"
 )
 
-var ptoRmap = map[utils.Pt]rune{
+var ptoRune = map[utils.Pt]rune{
 	{C: 0, R: 1}:  'v',
 	{C: -1, R: 0}: '<',
 	{C: 0, R: -1}: '^',
 	{C: 1, R: 0}:  '>',
-}
-
-type pair [2]rune
-
-type solution struct {
-	input      []string
-	numG, dirG utils.Grid[rune]
-	seen       map[utils.Pt]bool
-	// nPath        []utils.Pt
-	// nRes         [4][][]utils.Pt
-	ans int
-	// Nshorted     [4]int
-	shortest       int
-	pathsBylen     map[int][][]rune
-	path           []rune
-	rpMapN, rpMapD map[rune]utils.Pt
-	nPaths         map[pair][][]rune // start end// shortest paths
 }
 
 // func (s *solution) cleanNres() {
@@ -102,17 +85,14 @@ type solution struct {
 // 	s.seen[p] = false
 // }
 
-// update numberPath dict
-func (s *solution) all_num_paths(p utils.Pt, destRune rune) {
-	if !s.numG.IsInside(p) || s.seen[p] || s.numG.Get(p) == '#' {
+func (s *solution) find_paths(p utils.Pt, destRune rune, grid utils.Grid[rune]) {
+	if !grid.IsInside(p) || s.seen[p] || grid.Get(p) == '#' {
 		return
 	}
-	if s.numG.Get(p) == destRune {
-		if s.shortest == 0 || len(s.path) <= s.shortest {
-			s.shortest = len(s.path)
-			curPaths := s.pathsBylen[s.shortest]
-			curPaths = append(curPaths, slices.Clone(s.path))
-			s.pathsBylen[s.shortest] = curPaths
+	if grid.Get(p) == destRune {
+		if s.min == 0 || len(s.path) <= s.min {
+			s.min = len(s.path)
+			s.pathsByLen[s.min] = append(s.pathsByLen[s.min], string(s.path))
 		}
 		return
 	}
@@ -120,37 +100,164 @@ func (s *solution) all_num_paths(p utils.Pt, destRune rune) {
 	s.seen[p] = true
 	for _, dir := range utils.Dir4 {
 		nextP := p.PMove(dir)
-		s.path = append(s.path, ptoRmap[dir])
-		s.all_num_paths(nextP, destRune)
+		s.path = append(s.path, ptoRune[dir])
+		s.find_paths(nextP, destRune, grid)
 		s.path = s.path[:len(s.path)-1]
 	}
 	s.seen[p] = false
 }
 
-func (s *solution) run2() {
-	for _, line := range s.input {
-		line = "A" + line
-		for i := range len(line) - 1 {
-			start, end := rune(line[i]), rune(line[i+1])
+func (s *solution) buildMemo(line string, grid utils.Grid[rune], rToP map[rune]utils.Pt) {
+	for _, start := range line {
+		for _, end := range line {
+			pairs := pair{start, end}
+			if start == end {
+				s.memo[pairs] = []string{""}
+			}
 			// reinitial the status
-			s.shortest, s.pathsBylen = 0, map[int][][]rune{}
-			s.all_num_paths(s.rpMapN[start], end)
-			// fmt.Printf("%c, %c \n", start, end)
-			// fmt.Printf("%#v \n", s.shortestPath)
-			s.nPaths[pair{start, end}] = s.pathsBylen[s.shortest]
-		}
-	}
-	fmt.Printf("part2 %#v \n", s.nPaths)
-	for k, v := range s.nPaths {
-		fmt.Printf("%c, %c \n", k[0], k[1])
-		for _, line := range v {
-			fmt.Println(string(line))
+			s.min, s.pathsByLen = 0, map[int][]string{}
+			s.find_paths(rToP[start], end, grid)
+			s.memo[pairs] = s.pathsByLen[s.min]
 		}
 	}
 }
 
+func (s *solution) aggred(str string) []string {
+	str = "A" + str
+	res, path := []string{}, []string{}
+	var dfs func(int)
+	dfs = func(level int) {
+		if level == len(str)-1 {
+			res = append(res, strings.Join(path, "A")+"A")
+			return
+		}
+		start, end := rune(str[level]), rune(str[level+1])
+		for _, pattern := range s.memo[pair{start, end}] {
+			path = append(path, pattern)
+			dfs(level + 1)
+			path = path[:len(path)-1]
+		}
+	}
+	dfs(0)
+	return res
+}
+
+func (s *solution) dfsmin(str string, blink, target int) int {
+	if blink == target {
+		return len(str)
+	}
+	id := step{str, blink}
+	if v, ok := s.blinkMemo[id]; ok {
+		return v
+	}
+	str = "A" + str // for range not for build path
+	// expand
+	ans := 0
+	for i := range len(str) - 1 {
+		start, end := rune(str[i]), rune(str[i+1])
+
+		res := 0
+		for _, path := range s.memo[pair{start, end}] {
+			temp := s.dfsmin(path+"A", blink+1, target)
+			if res == 0 || temp < res {
+				res = temp
+			}
+		}
+
+		ans += res
+	}
+	s.blinkMemo[id] = ans
+	return ans
+}
+
+func (s *solution) run2() {
+	// build grid and r to pt map
+	s.numG.Array, s.runeToPtNum = s.buildGrid(4, 3, "789456123#0A")
+	s.dirG.Array, s.runeToPtDir = s.buildGrid(2, 3, "#^A<v>")
+	// for _, row := range s.numG.Array {
+	// 	fmt.Println(string(row))
+	// }
+
+	// build memo
+	s.buildMemo("7894561230A", s.numG, s.runeToPtNum)
+	s.buildMemo("^A<v>", s.dirG, s.runeToPtDir)
+
+	// for each line build paths
+	// for _, r1Line := range s.input {
+	// 	min := 0
+	// 	r1 := s.aggred(r1Line)
+	//
+	// 	for _, line := range r1 {
+	// 		// fmt.Println("r2", line)
+	// 		r2 := s.aggred(line)
+	// 		for _, line := range r2 {
+	// 			r3 := s.aggred(line)
+	// 			for _, v := range r3 {
+	// 				if min == 0 || len(v) < min {
+	// 					min = len(v)
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// 	s.ans += min * utils.IntsFromString(r1Line)[0]
+	// }
+	fmt.Println("original", s.ans)
+	res2 := 0
+	for _, r1Line := range s.input {
+		res := s.dfsmin(r1Line, 0, 26)
+		res *= utils.IntsFromString(r1Line)[0]
+		res2 += res
+	}
+	fmt.Println("dfs res", res2)
+	// for k, v := range s.memo {
+	// 	fmt.Printf("%cto%c \n", k[0], k[1])
+	// 	for _, line := range v {
+	// 		fmt.Print(string(line), ",")
+	// 	}
+	// 	fmt.Println("")
+	// }
+}
+
 func (s *solution) res() int {
 	return s.ans
+}
+
+type (
+	pair [2]rune
+	step struct {
+		str   string
+		blink int
+	}
+	solution struct {
+		input                    []string
+		r1, r2, r3               []string
+		numG, dirG               utils.Grid[rune]
+		seen                     map[utils.Pt]bool
+		ans                      int
+		min                      int
+		pathsByLen               map[int][]string
+		path                     []rune
+		runeToPtNum, runeToPtDir map[rune]utils.Pt
+		memo                     map[pair][]string // start end// shortest paths
+		blinkMemo                map[step]int
+	}
+)
+
+func (s *solution) buildGrid(nrow, ncol int, str string) ([][]rune, map[rune]utils.Pt) {
+	grid := make([][]rune, nrow)
+	rtoP := map[rune]utils.Pt{}
+	count := 0
+	for r := range grid {
+		grid[r] = make([]rune, ncol)
+		for c := range grid[r] {
+			R := rune(str[count])
+			grid[r][c] = R
+			// record run pt map
+			rtoP[R] = utils.Pt{C: c, R: r}
+			count++
+		}
+	}
+	return grid, rtoP
 }
 
 func buildSolution(r io.Reader) *solution {
@@ -159,40 +266,11 @@ func buildSolution(r io.Reader) *solution {
 		log.Fatalf("could not read input: %v %v", lines, err)
 	}
 
-	numpab, dirctPad := "789456123#0A", "#^A<v>"
-	numGraph := make([][]rune, 4)
-	count := 0
-	numMap, DMap := map[rune]utils.Pt{}, map[rune]utils.Pt{}
-	for r := range numGraph {
-		numGraph[r] = make([]rune, 3)
-		for c := range numGraph[r] {
-			numGraph[r][c] = rune(numpab[count])
-			numMap[rune(numpab[count])] = utils.Pt{C: c, R: r}
-			count++
-		}
-	}
-	dirGraph := make([][]rune, 2)
-	count = 0
-	for r := range dirGraph {
-		dirGraph[r] = make([]rune, 3)
-		for c := range dirGraph[r] {
-			dirGraph[r][c] = rune(dirctPad[count])
-			DMap[rune(dirctPad[count])] = utils.Pt{C: c, R: r}
-			count++
-		}
-	}
 	return &solution{
-		input:  lines,
-		rpMapN: numMap,
-		rpMapD: DMap,
-		seen:   map[utils.Pt]bool{},
-		nPaths: map[pair][][]rune{},
-		numG: utils.Grid[rune]{
-			Array: numGraph,
-		},
-		dirG: utils.Grid[rune]{
-			Array: dirGraph,
-		},
+		input:     lines,
+		seen:      map[utils.Pt]bool{},
+		memo:      map[pair][]string{},
+		blinkMemo: map[step]int{},
 	}
 }
 

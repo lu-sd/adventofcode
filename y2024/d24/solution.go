5f07c90a063a0419f4b3d5171d61d69a97705123
lu-sd <luhou.me@gmail.com>
2024-12-27 16:07:46 -0800

    d25 haha

--- y2024/d24/solution.go
+++ y2024/d24/solution.go
@@ -6,17 +6,151 @@ import (
 	"io"
 	"log"
 	"os"
+	"sort"
+	"strconv"
+	"strings"
 	"time"
 )
 
+func binaryToDecimal(binary []rune) int {
+	res := 0
+	for _, char := range binary {
+		res *= 2
+		res += int(char - '0')
+	}
+	// binary := strconv.FormatInt(int64(decimal), 2)
+	return res
+}
+
 func (s *solution) run1() {
+	s.buildGaph(s.graphlines)
+	s.buildInDegree()
+	sorted := s.topSort()
+	// fmt.Printf("%#v \n", s.inDegree)
+	// fmt.Printf("%#v \n", s.graph)
+	// fmt.Printf("%#v \n", sorted)
+	// fmt.Printf("%v \n", s.vertices)
+	// for k, v := range s.vertices {
+	// 	fmt.Printf("id %s %v \n", k, *v)
+	// }
+	// fmt.Println(s.inDegree)
+	for _, id := range sorted {
+		item := s.vertices[id]
+		item.process(s.vertices)
+	}
+	// collect all z related id
+	var res []string
+	for _, id := range sorted {
+		if id[0] == 'z' {
+			res = append(res, id)
+		}
+	}
+	sort.Strings(res)
+	// fmt.Printf("%#v \n", res)
+	binary := make([]rune, len(res))
+	for i, id := range res {
+		value := '0'
+		if s.vertices[id].res {
+			value = '1'
+		}
+		fmt.Println("id", id, string(value))
+		binary[len(res)-i-1] = value
+	}
+	fmt.Println(string(binary))
+	fmt.Println(binaryToDecimal(binary))
 }
 
 func (s *solution) run2() {
+	s.buildGaph(s.graphlines)
+	s.buildInDegree()
+	sorted := s.topSort()
+	// fmt.Printf("%#v \n", s.inDegree)
+	// fmt.Printf("%#v \n", s.graph)
+	// fmt.Printf("%#v \n", sorted)
+	// fmt.Printf("%v \n", s.vertices)
+	// for k, v := range s.vertices {
+	// 	fmt.Printf("id %s %v \n", k, *v)
+	// }
+	// fmt.Println(s.inDegree)
+	for _, id := range sorted {
+		item := s.vertices[id]
+		item.process(s.vertices)
+	}
+	// collect all z related id
+	var res []string
+	for _, id := range sorted {
+		if id[0] == 'z' {
+			res = append(res, id)
+		}
+	}
+	sort.Strings(res)
+	// fmt.Printf("%#v \n", res)
+	binary := make([]rune, len(res))
+	for i, id := range res {
+		value := '0'
+		if s.vertices[id].res {
+			value = '1'
+		}
+		fmt.Println("id", id, string(value))
+		binary[len(res)-i-1] = value
+	}
+	fmt.Println(string(binary))
+	fmt.Println(binaryToDecimal(binary))
+	z := strconv.FormatInt(int64(s.expectedZ), 2)
+
+	fmt.Println("extected z:", s.expectedZ)
+	fmt.Println("extected:", z)
 }
 
 func (s *solution) res() int {
 	return s.ans
+}
+
+func (s *solution) buildInDegree() {
+	inDegree := map[string]int{}
+	for _, connects := range s.graph {
+		for _, id := range connects {
+			// add count for each small island
+			inDegree[id]++ // dont care who point to this id, only count matter
+			// x00 may miss, default is 0. which is expected
+		}
+	}
+	s.inDegree = inDegree
+}
+
+func (s *solution) topSort() []string {
+	var result, queue []string
+
+	for id := range s.vertices {
+		if s.inDegree[id] == 0 {
+			queue = append(queue, id)
+		}
+	}
+	for len(queue) > 0 {
+		id := queue[0]
+		queue = queue[1:]
+		result = append(result, id)
+		for _, child := range s.graph[id] {
+			s.inDegree[child]--
+			if s.inDegree[child] == 0 {
+				queue = append(queue, child)
+			}
+		}
+	}
+	return result
+}
+
+func (s *solution) buildGaph(lines []string) {
+	graph := map[string][]string{}
+	for _, line := range lines {
+		var a, op, b, id string
+		fmt.Sscanf(line, "%s %s %s -> %s", &a, &op, &b, &id)
+		// expand graph
+		s.vertices[id] = &vertex{a: a, b: b, op: op, id: id}
+		graph[a] = append(graph[a], id)
+		graph[b] = append(graph[b], id)
+	}
+	s.graph = graph
 }
 
 func buildSolution(r io.Reader) *solution {
@@ -24,15 +158,78 @@ func buildSolution(r io.Reader) *solution {
 	if err != nil {
 		log.Fatalf("could not read input: %v %v", lines, err)
 	}
+	// graph := map[string][]string{}
+	// inDegree := map[string]int{}
+	part1 := true
+	var graphLines []string
+	vertices := map[string]*vertex{} // normal only need string list
+	var allX, allY string
+	for _, line := range lines {
+		if len(line) == 0 {
+			part1 = false
+			continue
+		}
+		if part1 {
+			items := strings.Split(line, ": ")
+			// fmt.Sscanf(line, "%s: %d", &id, &v)
+			vertices[items[0]] = &vertex{res: items[1] == "1"}
+			if items[0][0] == 'x' {
+				allX = items[1] + allX
+			}
+			if items[0][0] == 'y' {
+				allY = items[1] + allY
+			}
+			// graph[id] = nil // not need to be nil
+		} else {
+			graphLines = append(graphLines, line)
+		}
+	}
+	fmt.Println("x", allX, "y", allY)
+	fmt.Println("z", binaryToDecimal([]rune(allX))+binaryToDecimal([]rune(allY)))
+	// vertices[""] = &vertex{}
 
 	return &solution{
-		ans: 0,
+		// graph:    graph,
+		expectedZ:  binaryToDecimal([]rune(allX)) + binaryToDecimal([]rune(allY)),
+		vertices:   vertices,
+		graphlines: graphLines,
 	}
 }
 
-type solution struct {
-	ans int
+func (v *vertex) process(dict map[string]*vertex) {
+	var a, b bool
+	if vertex, ok := dict[v.a]; ok {
+		a = vertex.res
+	}
+	if vertex, ok := dict[v.b]; ok {
+		b = vertex.res
+	}
+	switch v.op {
+	case "AND":
+		v.res = a && b
+	case "XOR":
+		v.res = a != b
+	case "OR":
+		v.res = a || b
+	default:
+	}
 }
+
+type (
+	vertex struct {
+		a, b, id, op string
+		res          bool
+	}
+	solution struct {
+		graphlines    []string
+		expectedZ     int
+		graph         map[string][]string
+		inDegree      map[string]int
+		ans           int
+		vertices      map[string]*vertex
+		sorted, queue []string
+	}
+)
 
 func part1(r io.Reader) int {
 	s := buildSolution(r)
--- y2024/d25/solution.go
+++ y2024/d25/solution.go
@@ -10,6 +10,22 @@ import (
 )
 
 func (s *solution) run1() {
+	for _, key := range s.pairs['.'] {
+		for _, lock := range s.pairs['#'] {
+			if s.valid(key, lock) {
+				s.ans++
+			}
+		}
+	}
+}
+
+func (s *solution) valid(a, b []int) bool {
+	for i, v := range a {
+		if v+b[i] > 5 {
+			return false
+		}
+	}
+	return true
 }
 
 func (s *solution) run2() {
@@ -19,19 +35,61 @@ func (s *solution) res() int {
 	return s.ans
 }
 
+func Lockvalue(runeG [][]rune) []int {
+	nrow, ncol := len(runeG), len(runeG[0])
+	res := make([]int, ncol)
+	for j := range ncol {
+		count := -1
+		for i := range nrow {
+			if runeG[i][j] == '#' {
+				count++
+			}
+		}
+		res[j] = count
+	}
+	return res
+}
+
 func buildSolution(r io.Reader) *solution {
 	lines, err := utils.LinesFromReader(r)
 	if err != nil {
 		log.Fatalf("could not read input: %v %v", lines, err)
 	}
+	pairs := map[rune][][]int{}
+	hint := '0'
+	var runeG [][]rune
+	for _, line := range lines {
+		if len(line) == 0 {
+			if len(runeG) > 0 {
+				pairs[hint] = append(pairs[hint], Lockvalue(runeG))
+			}
+			hint = '0'
+			runeG = runeG[:0]
+			continue
+		}
+		if line[0] == '#' && hint == '0' {
+			hint = '#'
+		}
+		if line[0] == '.' && hint == '0' {
+			hint = '.'
+		}
+		runeG = append(runeG, []rune(line))
+	}
+	if len(runeG) > 0 {
+		pairs[hint] = append(pairs[hint], Lockvalue(runeG))
+	}
+
+	// fmt.Printf("%#v", pairs)
 
 	return &solution{
-		ans: 0,
+		pairs: pairs,
+		ans:   0,
 	}
 }
 
 type solution struct {
-	ans int
+	pairs map[rune][][]int
+	ans   int
 }
 
 func part1(r io.Reader) int {

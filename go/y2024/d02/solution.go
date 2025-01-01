5c419051151ca52516e9dc86fb459c6529230f5c
lu-sd <luhou.me@gmail.com>
2024-12-31 19:21:35 -0800

    d02

--- go/y2024/d02/solution.go
+++ go/y2024/d02/solution.go
@@ -9,13 +9,54 @@ import (
 )
 
 type solution struct {
-	ans int
+	ans, ans2 int
+	nums      [][]int
+}
+
+func (s *solution) isSafe(list []int) bool {
+	diff := list[1] - list[0]
+	for i := 1; i < len(list); i++ {
+		curDif := list[i] - list[i-1]
+		if s.invalid(diff, curDif) {
+			return false
+		}
+	}
+	return true
+}
+
+func (s *solution) invalid(diff, curDif int) bool {
+	return diff*curDif <= 0 || utils.Abs(curDif) > 3
 }
 
 func (s *solution) run1() {
+	for _, numL := range s.nums {
+		if s.isSafe(numL) {
+			s.ans++
+		}
+	}
 }
 
 func (s *solution) run2() {
+	for _, numL := range s.nums {
+		if s.isSafe(numL) {
+			s.ans2++
+		} else {
+			for skip := 0; skip < len(numL); skip++ {
+				newNumL := []int{}
+				for i := 0; i < len(numL); i++ {
+					if i == skip {
+						continue
+					}
+					newNumL = append(newNumL, numL[i])
+				}
+				// newNumL := append(numL[:skip], numL[skip+1:]...) wrong
+				if s.isSafe(newNumL) {
+					s.ans2++
+					break
+				}
+			}
+		}
+	}
 }
 
 func (s *solution) res() int {
@@ -27,9 +68,15 @@ func buildSolution(r io.Reader) *solution {
 	if err != nil {
 		log.Fatalf("could not read input: %v %v", lines, err)
 	}
-
+	nums := [][]int{}
+	for _, line := range lines {
+		numList := utils.IntsFromString(line)
+		nums = append(nums, numList)
+	}
 	return &solution{
-		ans: 0,
+		ans:  0,
+		ans2: 0,
+		nums: nums,
 	}
 }
 
@@ -42,7 +89,7 @@ func part1(r io.Reader) int {
 func part2(r io.Reader) int {
 	s := buildSolution(r)
 	s.run2()
-	return s.res()
+	return s.ans2
 }
 
 func main() {

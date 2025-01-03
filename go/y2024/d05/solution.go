214b85d505947f44ecaee9bb850929675d27abf5
lu-sd <luhou.me@gmail.com>
2025-01-02 16:37:49 -0800

    d05 toplogicSort

--- go/y2024/d05/solution.go
+++ go/y2024/d05/solution.go
@@ -6,16 +6,106 @@ import (
 	"io"
 	"log"
 	"os"
+	"slices"
 )
 
-type solution struct {
-	ans int
-}
+type (
+	adj      map[int][]int
+	solution struct {
+		ans     int
+		adjList adj
+		manuals [][]int
+	}
+)
 
 func (s *solution) run1() {
+	for _, manual := range s.manuals {
+		if s.isRightOrder(manual) {
+			s.ans += s.middle(manual)
+		}
+	}
+}
+
+func (s *solution) isRightOrder(m []int) bool {
+	seen := map[int]bool{}
+	for _, num := range m {
+		for _, after := range s.adjList[num] {
+			if seen[after] {
+				return false
+			}
+		}
+		seen[num] = true
+	}
+	return true
+}
+
+func (s *solution) middle(m []int) int {
+	return m[len(m)/2]
 }
 
 func (s *solution) run2() {
+	for _, manual := range s.manuals {
+		if !s.isRightOrder(manual) {
+			s.ans += s.resorted(manual)
+		}
+	}
+}
+
+func (s *solution) buildList(task []int) adj {
+	nAdjList := make(adj)
+	for _, par := range task {
+		dependcy := []int{}
+		for _, num := range s.adjList[par] {
+			if slices.Contains(task, num) {
+				dependcy = append(dependcy, num)
+			}
+		}
+		nAdjList[par] = dependcy
+	}
+	return nAdjList
+}
+
+func findIndegre(task []int, nAdjList adj) map[int]int {
+	inDegree := make(map[int]int)
+	for _, dependcy := range nAdjList {
+		for _, num := range dependcy {
+			inDegree[num]++
+		}
+	}
+	return inDegree
+}
+
+func validAdj(m []int, indegree map[int]int, nAdjList adj) int {
+	goodList := []int{}
+	queue := []int{}
+	for _, node := range m {
+		if indegree[node] == 0 {
+			queue = append(queue, node)
+		}
+	}
+
+	for len(queue) > 0 {
+		head := queue[0]
+		queue = queue[1:]
+		goodList = append(goodList, head)
+		for _, num := range nAdjList[head] {
+			indegree[num]--
+			if indegree[num] == 0 {
+				queue = append(queue, num)
+			}
+		}
+	}
+	return goodList[len(goodList)/2]
+}
+
+func (s *solution) resorted(m []int) int {
+	// buildList
+	nAdjList := s.buildList(m)
+	// findIndegre
+	indegree := findIndegre(m, nAdjList)
+	// validList
+	res := validAdj(m, indegree, nAdjList)
+	return res
 }
 
 func (s *solution) res() int {
@@ -27,9 +117,25 @@ func buildSolution(r io.Reader) *solution {
 	if err != nil {
 		log.Fatalf("could not read input: %v %v", lines, err)
 	}
-
+	processAdj := true
+	adjList := make(adj)
+	manuals := make([][]int, 0)
+	for _, line := range lines {
+		nums := utils.IntsFromString(line)
+		if len(line) == 0 {
+			processAdj = false
+			continue
+		}
+		if processAdj {
+			adjList[nums[0]] = append(adjList[nums[0]], nums[1])
+		} else {
+			manuals = append(manuals, nums)
+		}
+	}
 	return &solution{
-		ans: 0,
+		ans:     0,
+		adjList: adjList,
+		manuals: manuals,
 	}
 }
 

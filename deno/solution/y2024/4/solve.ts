ece1e855606a5b88de5009b3c2318c1f6d9865a1
lu-sd <luhou.me@gmail.com>
2025-01-05 13:03:13 -0800

    add d5

---
+++ deno/solution/y2024/4/solve.ts
@@ -0,0 +1,74 @@
+import { dirname, join } from "@std/path";
+import { Grid } from "../../../lib/dataStructure.ts";
+
+export class solution {
+  input: string;
+  lines: string[];
+  ans = 0;
+  grid: Grid<string>;
+
+  constructor(input: string) {
+    this.input = input;
+    this.lines = input.split("\n");
+    const array: string[][] = [];
+    for (const [r, line] of this.lines.entries()) {
+      array[r] = Array(line.length);
+      for (let c = 0; c < line.length; c++) {
+        const char = line[c];
+        array[r][c] = char;
+      }
+    }
+    // const array:string[][] = this.lines.map(line => [...line])
+    this.grid = new Grid(array);
+  }
+  part1() {
+    const target = "XMAS";
+    const dirs8 = [];
+    for (let x = -1; x <= 1; x++) {
+      for (let y = -1; y <= 1; y++) {
+        if (x == 0 && y == 0) {
+          continue;
+        }
+        dirs8.push([x, y]);
+      }
+    }
+    console.log(dirs8);
+    for (let r = 0; r < this.grid.nrow; r++) {
+      for (let c = 0; c < this.grid.nclo; c++) {
+        for (const dir of dirs8) {
+          let valid = true;
+          for (let i = 0; i < target.length; i++) {
+            const nr = r + i * dir[0];
+            const nc = c + i * dir[1];
+            if (
+              !this.grid.isInside(nr, nc) ||
+              this.grid.getChar(nr, nc) !== target[i]
+            ) {
+              valid = false;
+              break;
+            }
+          }
+          if (valid) {
+            this.ans++;
+          }
+        }
+      }
+    }
+  }
+  part2() {}
+  res(): number {
+    return this.ans;
+  }
+}
+
+export default function run() {
+  const __dirname = dirname(import.meta.url);
+  const filePath = new URL(join(__dirname, "input.txt"));
+  const input = Deno.readTextFileSync(filePath).trim();
+  const s1 = new solution(input);
+  s1.part1();
+  console.log("Part1 result ->", s1.res());
+  const s2 = new solution(input);
+  s2.part2();
+  console.log("Part2 result ->", s2.res());
+}

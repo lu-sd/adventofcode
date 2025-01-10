8226192f8fddbd9d83b6a10b07179816eca6a24f
lu-sd <luhou.me@gmail.com>
2025-01-09 18:04:14 -0800

    d9 part2 polish basic func

--- deno/solution/y2024/9/solve.ts
+++ deno/solution/y2024/9/solve.ts
@@ -10,11 +10,18 @@ export class solution {
     const arr: number[] = [];
     let id = 0;
     for (let i = 0; i < this.input.length; i++) {
+      let count = Number(this.input[i]);
       if (i % 2 === 0) {
-        arr.push(...Array(Number(this.input[i])).fill(id));
+        while (count--) {
+          arr.push(id);
+        }
+        // arr.push(...Array(count).fill(id));
         id++;
       } else {
-        arr.push(...Array(Number(this.input[i])).fill(-1));
+        while (count--) {
+          arr.push(-1);
+        }
+        // arr.push(...Array(count).fill(-1));
       }
     }
     this.narr = arr;
@@ -37,13 +44,70 @@ export class solution {
         r--;
         continue;
       }
-      [this.narr[l], this.narr[r]] = [this.narr[r], this.narr[l]];
-      // this.swap(l, r);
+      // [this.narr[l], this.narr[r]] = [this.narr[r], this.narr[l]];
+      this.swap(l, r);
       l++;
       r--;
     }
   }
-  part2() {}
+
+  findFile(id: number): [number, number] {
+    let fLeft = -1;
+    let len = 0;
+    for (const [i, v] of this.narr.entries()) {
+      if (v === id) {
+        if (fLeft == -1) {
+          fLeft = i;
+        }
+        len++;
+      }
+    }
+    return [fLeft, len];
+  }
+
+  findFirstSpot(end: number, len: number): [number, boolean] {
+    let curlen = 0;
+    let start = -1;
+    for (let i = 0; i < end; i++) {
+      if (this.narr[i] === -1) {
+        if (curlen === 0) {
+          start = i;
+        }
+        curlen++;
+        if (curlen === len) {
+          return [start, true];
+        }
+        continue;
+      }
+
+      curlen = 0;
+      start = -1;
+    }
+    return [-1, false];
+  }
+
+  part2() {
+    for (let id = this.narr[this.narr.length - 1]; id >= 0; id--) {
+      const [fileS, fLen] = this.findFile(id);
+      const [freeS, find] = this.findFirstSpot(fileS, fLen);
+      if (find) {
+        this.swap2(freeS, fileS, fLen, id);
+        // for (let i = 0; i < fLen; i++) {
+        //   this.narr[fSart + i] = id;
+        //   this.narr[fEdge + i] = -1;
+        // }
+      }
+    }
+  }
+  swap2(freeS: number, fileS: number, len: number, fileid: number) {
+    for (let idx = freeS; idx < freeS + len; idx++) {
+      this.narr[idx] = fileid;
+    }
+    for (let idx = fileS; idx < fileS + len; idx++) {
+      this.narr[idx] = -1;
+    }
+  }
+
   res(): number {
     for (let i = 0; i < this.narr.length; i++) {
       const ele = this.narr[i];

0e82d7f4521df35c0710cf9f7f6f2389b4bd8e85
lu-sd <luhou.me@gmail.com>
2025-01-11 21:38:28 -0800

    d11

--- go/y2024/d11/solution.go
+++ go/y2024/d11/solution.go
@@ -6,10 +6,46 @@ import (
 	"io"
 	"log"
 	"os"
+	"strconv"
 	"time"
 )
 
 func (s *solution) run1() {
+	for _, v := range s.nums {
+		s.ans += s.dfs(v, 0, 75)
+	}
+}
+
+func (s *solution) dfs(stone, cur, target int) int {
+	if cur == target {
+		return 1
+	}
+	id := fmt.Sprintf("%d,%d", stone, cur)
+	if v, ok := s.memo[id]; ok {
+		return v
+	}
+	newStones := s.blink(stone)
+	ans := 0
+	for _, v := range newStones {
+		ans += s.dfs(v, cur+1, target)
+	}
+	s.memo[id] = ans
+	return ans
+}
+
+func (s *solution) blink(stone int) (newStones []int) {
+	if stone == 0 {
+		newStones = append(newStones, 1)
+	} else if len(strconv.Itoa(stone))%2 == 0 {
+		numStr := strconv.Itoa(stone)
+		half := len(numStr) / 2
+		left, _ := strconv.Atoi(numStr[:half])
+		right, _ := strconv.Atoi(numStr[half:])
+		newStones = append(newStones, left, right)
+	} else {
+		newStones = append(newStones, stone*2024)
+	}
+	return
 }
 
 func (s *solution) run2() {
@@ -29,13 +65,18 @@ func buildSolution(r io.Reader) *solution {
 		log.Fatalf("could not read input: %v %v", lines, err)
 	}
 
+	nums := utils.IntsFromString(lines[0])
 	return &solution{
-		ans: 0,
+		ans:  0,
+		nums: nums,
+		memo: map[string]int{},
 	}
 }
 
 type solution struct {
-	ans int
+	ans  int
+	nums []int
+	memo map[string]int
 }
 
 func part1(r io.Reader) int {
@@ -46,8 +87,8 @@ func part1(r io.Reader) int {
 
 func part2(r io.Reader) int {
 	s := buildSolution(r)
-	s.run2()
-	return s.res2()
+	s.run1()
+	return s.res1()
 }
 
 func main() {

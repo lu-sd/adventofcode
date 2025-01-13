26809a6ea86384129fc60a4a650fc139ef6c72c7
lu-sd <luhou.me@gmail.com>
2025-01-13 15:56:25 -0800

    d13 in  ts should use new Point to init class

--- deno/solution/y2024/13/solve.ts
+++ deno/solution/y2024/13/solve.ts
@@ -1,15 +1,70 @@
 import { dirname, join } from "@std/path";
-
+import { Point } from "../../../lib/dataStructure.ts";
+import { IntsFromString } from "../../../lib/number.ts";
+type machine = {
+  a: Point;
+  b: Point;
+  target: Point;
+};
 export class solution {
   input: string;
   lines: string[];
   ans = 0;
+  machines: machine[];
 
   constructor(input: string) {
     this.input = input;
     this.lines = input.split("\n");
+    const machines: machine[] = [];
+    const pts: Point[] = [];
+    for (const [i, line] of this.lines.entries()) {
+      const nums = IntsFromString(line);
+      if (i % 4 === 3) {
+        machines.push({ a: pts[0], b: pts[1], target: pts[2] });
+        continue;
+      }
+      pts[i % 4] = new Point(0, 0);
+      pts[i % 4].x = nums[0];
+      pts[i % 4].y = nums[1];
+    }
+    machines.push({ a: pts[0], b: pts[1], target: pts[2] });
+    this.machines = machines;
   }
-  part1() {}
+  part1() {
+    for (const m of this.machines) {
+      this.ans += this.dfs(m.a, m.b, m.target);
+    }
+  }
+
+  found(
+    aNum: number,
+    bNum: number,
+    a: Point,
+    b: Point,
+    target: Point,
+  ): boolean {
+    return aNum * a.x + bNum * b.x === target.x &&
+      aNum * a.y + bNum * b.y === target.y;
+  }
+
+  dfs(a: Point, b: Point, target: Point): number {
+    let lowest = 0;
+    let first = true;
+    for (let aNum = 0; aNum < 100; aNum++) {
+      for (let bNum = 0; bNum < 100; bNum++) {
+        if (this.found(aNum, bNum, a, b, target)) {
+          const curL = aNum * 3 + bNum;
+          if (first) {
+            lowest = curL;
+            first = false;
+          } else {
+            lowest = Math.min(lowest, curL);
+          }
+        }
+      }
+    }
+    return lowest;
+  }
   part2() {}
   res(): number {
     return this.ans;
